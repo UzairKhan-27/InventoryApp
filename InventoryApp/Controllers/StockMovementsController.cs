831bb1683d05fe2cc43d9c5f11a8711da8147947
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2025-03-30 12:54:31 +0500

    add stock movement methods

---
+++ InventoryApp/Controllers/StockMovementsController.cs
@@ -0,0 +1,50 @@
+﻿using InventoryApp.Data;
+using InventoryApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StockMovementsController : ControllerBase
+{
+    private readonly ProductContext dbContext;
+
+    public StockMovementsController(ProductContext dbContext )
+    {
+        this.dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public ActionResult<List<StockMovement>> GetStockMovements()
+    {
+        var stockMovements = dbContext.StockMovements.ToList();
+        return Ok(stockMovements);
+    }
+    [HttpGet("{id}")]
+    public ActionResult<StockMovement> GetStockMovement(Guid id)
+    {
+        var stockMovement = dbContext.StockMovements.Find(id);
+        if (stockMovement == null)
+        {
+            return NotFound($"Stock movement with ID {id} not found.");
+        }
+        return Ok(stockMovement);
+    }
+    [HttpPost]
+    public ActionResult<StockMovement> AddStockMovement([FromBody] AddStockMovementDto addStockMovementDto)
+    {
+        var stockMovement = new StockMovement()
+        {
+            ProductId = addStockMovementDto.ProductId,
+            Type = addStockMovementDto.Type,
+            Count = addStockMovementDto.Count
+        };
+        dbContext.StockMovements.Add(stockMovement);
+        dbContext.SaveChanges();
+        return Ok(stockMovement);
+    }
+
+}
--- InventoryApp/Models/AddStockMovementDto.cs
+++ InventoryApp/Models/AddStockMovementDto.cs
@@ -1,18 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace InventoryApp.Models;
 
-public class StockMovement
+public class AddStockMovementDto
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
     public Guid ProductId { get; set; }
     [RegularExpression("^(stocked in|sold|removed)$", ErrorMessage = "Chooose between stocked in, sold and removed")]
     public string Type { get; set; }
     public int Count { get; set; }
-    public DateTime Timestamp { get; set; } = DateTime.Now;
-    [JsonIgnore]
-    public Product Product { get; set;}
-
-
 }

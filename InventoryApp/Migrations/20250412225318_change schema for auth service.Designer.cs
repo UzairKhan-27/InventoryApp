9f537724f2091c1f2ff93ef944cb833b175820ae
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2025-04-13 03:59:51 +0500

    update migration

--- InventoryApp/Migrations/20250412225318_change schema for auth service.Designer.cs
+++ InventoryApp/Migrations/20250412225318_change schema for auth service.Designer.cs
@@ -4,6 +4,7 @@ using InventoryApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InventoryApp.Migrations
 {
     [DbContext(typeof(ProductContext))]
-    partial class ProductContextModelSnapshot : ModelSnapshot
+    [Migration("20250412225318_change schema for auth service")]
+    partial class changeschemaforauthservice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

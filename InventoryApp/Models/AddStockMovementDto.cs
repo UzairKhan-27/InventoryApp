1dfef690abb50449aeb6de30970530752f286406
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2025-04-13 00:17:43 +0500

    add store id property

--- InventoryApp/Models/AddStockMovementDto.cs
+++ InventoryApp/Models/AddStockMovementDto.cs
@@ -4,6 +4,7 @@ namespace InventoryApp.Models;
 
 public class AddStockMovementDto
 {
+    public Guid StoreId { get; set; }
     public Guid ProductId { get; set; }
     [RegularExpression("^(stocked in|sold|removed)$", ErrorMessage = "Chooose between stocked in, sold and removed")]
     public string Type { get; set; }
